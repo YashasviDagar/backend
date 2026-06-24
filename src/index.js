5b5ae5eb4860070a80d920fdd0be082934db9bb2
Yashasvi Dagar <dagaryashu2046@gmail.com>
2026-06-24 23:01:43 +0530

    Connecting to DB, DNS problem

---
+++ src/index.js
@@ -0,0 +1,40 @@
+// require('dotenv').config()
+import dns from "dns";
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import {DB_NAME} from './constants.js'
+import connectDB from "./db/index.js"
+
+// dotenv.config({
+//     path: './.env'
+// })
+
+
+connectDB()
+
+console.log("PORT =", process.env.PORT)
+console.log("MONGODB_URI =", process.env.MONGODB_URI)
+
+
+// import express from 'express' 
+// const app = express()
+// ;( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("errror", (error) => {
+//             console.log("ERRR: ", error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         })
+
+//     } catch (error) {
+//         console.error("ERROR: ", error)
+//         throw err
+//     }
+// } )()
